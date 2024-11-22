43cc057cd2a71283856cdcc8372b3b54c597e278
Logan Rawlins <maveric@gotskill.net>
2024-11-22 13:04:46 -0700

    removing missed --reboot and removing libprintf first

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ rpm-ostree install screen
 
 #add libfprint-tod-goodix repo an override libfprint with libfprint-tod-goodix
 curl -o /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9.repo "https://copr.fedorainfracloud.org/coprs/antiderivative/libfprint-tod-goodix-0.0.9/repo/fedora-$(rpm -E %fedora)/antiderivative-libfprint-tod-goodix-0.0.9-fedora-$(rpm -E %fedora).repo"
-rpm-ostree override replace --experimental --freeze --from repo=copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9 libfprint-tod --reboot
 rpm-ostree override remove libfprint
+rpm-ostree override replace --experimental --freeze --from repo=copr:copr.fedorainfracloud.org:antiderivative:libfprint-tod-goodix-0.0.9 libfprint-tod
 rpm-ostree install libfprint-tod-goodix
 
 # this would install a package from rpmfusion
